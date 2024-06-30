572db167dcf26b2394a494af59fda5ded2d0c333
Ignacio Hernández <ignaciohrdz@gmail.com>
2024-06-30 14:18:11 +0200

    Training on the LIVE-IQA database release 2

--- README.md
+++ README.md
@@ -9,15 +9,16 @@ The full details of SSEQ can be found in the paper: [No-reference image quality 
 Vectorized implementation of:
 
 - Patch spatial entropy
-- DCT for spectral entropy
+- DCT for spectral entropy (more info [here](https://eng.libretexts.org/Bookshelves/Electrical_Engineering/Signal_Processing_and_Modeling/Information_and_Entropy_(Penfield)/03%3A_Compression/3.08%3A_Detail-_2-D_Discrete_Cosine_Transformation/3.8.02%3A_Discrete_Cosine_Transformation))
 
 ## Results
 
 Every dataset was split into a training and a test set. I used the training sets with K-fold cross-validation to get the best parameters for each SVR model. The following are the results on each test set:
 
-| Dataset  | LCC    | SROCC  |
-|----------|--------|--------|
-| csiq     | 0,8493 | 0,7913 |
-| kadid10k | 0,6075 | 0,5716 |
-| koniq10k | 0,5745 | 0,5573 |
-| tid2013  | 0,7892 | 0,7204 |
+| Dataset  | LCC    | SROCC   |
+|----------|--------|---------|
+| csiq     | 0.8493 | 0.7913  |
+| kadid10k | 0.6075 | 0.5716  |
+| koniq10k | 0.5745 | 0.5573  |
+| liveiqa  | 0.8726 | 0.8713  |
+| tid2013  | 0.7892 | 0.7204  |
--- data.py
+++ data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from pathlib import Path
+import scipy
+import numpy as np
 
 
 def prepare_koniq(path_koniq: Path):
@@ -51,9 +53,39 @@ def prepare_tid(path_tid: Path):
     return dataset.loc[:, ["image_path", "image_name", "score"]]
 
 
+def prepare_liveiqa(path_liveiqa: Path):
+    """ Prepares the LIVE-IQA dataset (database release 2) for training """
+    # Loading the MATLAB file
+    path_dataset = path_liveiqa / "databaserelease2"
+    mat_file = scipy.io.loadmat(str(path_dataset / "dmos.mat"))
+    dmos = mat_file['dmos']
+    is_original = mat_file['orgs']
+
+    # The Readme.txt file from the database tells us the number of images per distortion
+    distortion_sizes = {
+        "jp2k": 227,
+        "jpeg": 233,
+        "wn": 174,
+        "gblur": 174,
+        "fastfading": 174
+    }
+    dataset = pd.DataFrame(columns=["image_path", "image_name", "score"])
+    offset = 0
+    for dist_name, n_imgs in distortion_sizes.items():
+        for i in range(n_imgs):
+            if is_original[0, offset + i] == 0:
+                image_name = "img{}.bmp".format(i+1)
+                image_path = path_dataset / dist_name / image_name
+                score = dmos[0, offset + i]
+                dataset.loc[len(dataset), :] = [image_path, image_name, score]
+        offset += n_imgs
+    return dataset
+
+
 dataset_fn_dict = {
     "koniq10k": prepare_koniq,
     "kadid10k": prepare_kadid,
     "csiq": prepare_csiq,
-    "tid2013": prepare_tid
+    "tid2013": prepare_tid,
+    "liveiqa": prepare_liveiqa
 }
